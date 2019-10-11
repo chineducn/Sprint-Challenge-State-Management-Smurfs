8d3eeff0299c698dcf89fc3e4ad651f543292ffd
unknown <neduvinci@gmail.com>
2019-10-11 09:39:29 +0100

    Completed creation of basic SmurfForm

--- smurfs/src/components/SmurfForm.jsx
+++ smurfs/src/components/SmurfForm.jsx
@@ -2,16 +2,56 @@ import React from 'react';
 import { Field, Form, Formik } from 'formik';
 
 export default function SmurfForm(props) {
+    const { onSubmit } = props;
+    const initialForm = {
+        name: '',
+        age: '',
+        height: '',
+    };
 
     return (
         <div>
             <Formik
-                initialValues
-                onSubmit
+                initialValues={initialForm}
+                onSubmit={onSubmit}
                 render={() => {
-                    
+                    return (
+                        <Form>
+                            <div>
+                                <label>
+                                    Name
+                                    <Field
+                                        name='name'
+                                        type='text'
+                                        placeholder='Name'
+                                    />
+                                </label>
+                            </div>
+                            <div>
+                                <label>
+                                    Age
+                                    <Field
+                                        name='age'
+                                        type='number'
+                                        placeholder='Age'
+                                    />
+                                </label>
+                            </div>
+                            <div>
+                                <label>
+                                    Height
+                                    <Field
+                                        name='height'
+                                        type='text'
+                                        placeholder='Height'
+                                    />
+                                </label>
+                            </div>
+                            <button type='submit'>Add Smurf</button>
+                        </Form>
+                    )
                 }}
             />
         </div>
     )
-}
+};
