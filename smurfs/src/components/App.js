e896351bdfc939224a6dfb8ab816bf9e05ce2e88
unknown <neduvinci@gmail.com>
2019-10-11 09:59:19 +0100

    In App, imported SmurfForm and passed the add smurf action

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { connect } from 'react-redux';
 
 import SmurfList from './SmurfList';
+import SmurfForm from './SmurfForm';
 
 import "./App.css";
 import * as actionCreators from '../state/actionCreators';
@@ -14,8 +15,18 @@ export class App extends Component {
         <div>Welcome to your state management version of Smurfs!</div>
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div>
-        <button onClick={this.props.getSmurfs}>Get Smurfs</button>
-        <SmurfList smurfList={this.props.smurfList} />
+        <button
+          onClick={this.props.getSmurfs}
+        >
+          Get Smurfs
+  
+        </button>
+        <SmurfForm
+          onSubmit={this.props.addSmurf}
+        />
+        <SmurfList
+          smurfList={this.props.smurfList}
+        />
       </div>
     );
   }
