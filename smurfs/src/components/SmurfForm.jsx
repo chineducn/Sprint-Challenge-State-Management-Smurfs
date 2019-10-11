556ccad005a526b24f26f163690b3c4f541ca24f
unknown <neduvinci@gmail.com>
2019-10-11 09:26:39 +0100

    In SmurfForm, creating basic Formik form

---
+++ smurfs/src/components/SmurfForm.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Field, Form, Formik } from 'formik';
+
+export default function SmurfForm(props) {
+
+    return (
+        <div>
+            <Formik
+                initialValues
+                onSubmit
+                render={() => {
+                    
+                }}
+            />
+        </div>
+    )
+}
