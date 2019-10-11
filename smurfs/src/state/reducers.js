ae2bcd2af26ca2f6cf59f198a40ac9b9cfd463e9
unknown <neduvinci@gmail.com>
2019-10-11 09:53:29 +0100

    Created case for adding smurf in smurfReducer

--- smurfs/src/state/reducers.js
+++ smurfs/src/state/reducers.js
@@ -2,11 +2,13 @@ import * as types from './actionTypes';
 
 const initialList = [];
 
-export function smurfReducer(smurflist = initialList, action) {
+export function smurfReducer(smurfList = initialList, action) {
     switch (action.type) {
         case types.GET_SMURFS:
             return action.payload;
+        case types.ADD_SMURF:
+            return smurfList.concat(action.payload);
         default:
-            return smurflist;
+            return smurfList;
     }
 };
