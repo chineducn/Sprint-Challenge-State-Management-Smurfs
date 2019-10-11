6b76362b4c30586b807d6d04b02fcb25db1c670c
unknown <neduvinci@gmail.com>
2019-10-11 09:17:35 +0100

    In App, connected App, imported SmurfList and rendered data from api to browser

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux';
+
+import SmurfList from './SmurfList';
+
 import "./App.css";
-class App extends Component {
+import * as actionCreators from '../state/actionCreators';
+
+export class App extends Component {
   render() {
     return (
       <div className="App">
@@ -8,9 +14,11 @@ class App extends Component {
         <div>Welcome to your state management version of Smurfs!</div>
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div>
+        <button onClick={this.props.getSmurfs}>Get Smurfs</button>
+        <SmurfList smurfList={this.props.smurfList} />
       </div>
     );
   }
 }
 
-export default App;
+export default connect(state => state, actionCreators, )(App);
